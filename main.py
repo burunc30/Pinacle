8f9899868c40e1e856d55a986762f90eade7fd91
burunc30 <e.necefov1985@gmail.com>
2025-06-24 20:38:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import asyncio
+from playwright.async_api import async_playwright
+from playwright_stealth import stealth_async
+from bs4 import BeautifulSoup
 
+URL = "https://www.pinnacle.com/en/odds/match/football"
+
+async def main():
+    print("🔗 Sayta daxil olunur...")
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await stealth_async(page)
+        await page.goto(URL, timeout=60000)
+        html = await page.content()
+        print("✅ HTML alındı.")
+
+        soup = BeautifulSoup(html, "html.parser")
+        title = soup.title.string if soup.title else "Başlıq yoxdur"
+        print(f"🌐 Səhifə Başlığı: {title}")
+
+        await browser.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
