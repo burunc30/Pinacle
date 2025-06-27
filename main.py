822f4317e70ee1aa619953dff10cd4db3b1f86ea
burunc30 <e.necefov1985@gmail.com>
2025-06-27 22:17:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import asyncio
 from playwright.async_api import async_playwright
 
 async def run():
-    print("🔗 Sayta daxil olunur: https://www.10bet.com/sports")
+    print("🔗 Sayta daxil olunur: https://www.188bet.com/en/sports")
     try:
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=True)
             context = await browser.new_context()
             page = await context.new_page()
-            await page.goto("https://www.10bet.com/sports", timeout=60000)
+            await page.goto("https://www.188bet.com/en/sports", timeout=60000)
             await page.wait_for_timeout(5000)
 
             html = await page.content()
             print("✅ HTML alındı.")
             print(f"ℹ️ Səhifə Başlığı: {await page.title()}")
 
-            # Burda test üçün neçə "div" var, onu sayırıq
             divs = await page.query_selector_all("div")
             print(f"🔢 Tapılan DIV sayı: {len(divs)}")
 
